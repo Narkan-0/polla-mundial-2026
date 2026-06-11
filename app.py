91863ab76d6e6cf53974891209678615e4c181da
Narkan-0 <rayoazul2012@icloud.com>
2026-06-11 19:24:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 1. CONFIGURACIÓN GENERAL
-PARTICIPANTES = ["Néstor", "Constanza", "Leonardo", "Renato", "Sergio", "José Mario", "José Alonso"]
+PARTICIPANTES = ["Néstor", "Constanza", "Leonardo", "Renato", "Sergio", "José Mario", "José Alonso", "Mario" ]
 CUOTA_INSCRIPCION = 5000
 PASSWORD_ADMIN = "admin123"
 
